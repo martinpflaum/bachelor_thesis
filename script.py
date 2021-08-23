032ed93d65572359e28b2f71ef7031a6aefab31d
Martin99y <martin99@users.noreply.github.com>
2021-08-23 17:14:53 +0100

    x

--- script.py
+++ script.py
@@ -25,13 +25,14 @@ def run_x5all(task,lr=None):
 
 
 
-run_x5all(task = 'edges',lr=0.0002)
-"""
-run_x5all(task = 'world_normal')
-
-args = {'skip_training': False, 'train_gan': False, 'sub_folder' : "1miocubes", 'stable_loss': True, 'calc_importance': True, 'input_dim': 32768, 'task': 'scene_depth', 'pretrained_gen': None, 'learn_save_file': None, 'file_name': None, 'results_folder': 'results'}
+args = {'skip_training': False, 'train_gan': False, 'sub_folder' : "1miocubes", 'stable_loss': True, 'calc_importance': True, 'input_dim': 4096, 'task': 'scene_depth', 'pretrained_gen': None, 'learn_save_file': None, 'file_name': None, 'results_folder': 'results'}
 args["pretrained_gen"] = "1miocubes.pkl"
 run_all(args)
+
+"""
+run_x5all(task = 'edges',lr=0.0002)
+
+run_x5all(task = 'world_normal')
 
 run_x5all(task = 'scene_depth')
 """
