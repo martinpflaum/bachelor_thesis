9ccb9987adde778ab249421f687872ec3c70c05c
martinpflaum <63510484+martinpflaum@users.noreply.github.com>
2021-09-06 23:17:41 +0200

    x

--- script.py
+++ script.py
@@ -48,17 +48,16 @@ def run_x5all(task,lr=None):
 
 
 
-"""
 args = {'skip_training': False, 'train_gan': False, 'sub_folder' : "1miocubes", 'stable_loss': True, 'calc_importance': True, 'input_dim': 4096, 'task': 'scene_depth', 'pretrained_gen': None, 'learn_save_file': None, 'file_name': None, 'results_folder': 'results'}
 args["pretrained_gen"] = "1miocubes.pkl"
 run_all(args)
 
-run_x5all(task = 'edges',lr=0.0002)
+#run_x5all(task = 'edges',lr=0.0002)
 
-run_x5all(task = 'world_normal')
+#run_x5all(task = 'world_normal')
 
 run_x5all(task = 'scene_depth')
-"""
+
 args = {'skip_training': False, 'train_gan': True, 'sub_folder' : "input_is_output_trgan(True)", 'stable_loss': False, 'calc_importance': False, 'input_dim': 4096, 'task': 'scene_depth', 'pretrained_gen': None, 'learn_save_file': None, 'file_name': None, 'results_folder': 'results'}
 args["make_input_depth"] = True
 run_all(args)
